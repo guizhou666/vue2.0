1d922a39598a26de2b5f2ee9cc51ad5cf6697094
yangguizhou <1836070453@qq.com>
2021-12-30 09:54:32 +0800

    update:

--- src/view/pie/index.vue
+++ src/view/pie/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="pie-content">
     <div class="li-pie">
-      <ufast-pie :pieConfig="pieConfig2" @change="change"></ufast-pie>
+      <ufast-pie :pieConfig="pieConfig" @change="change"></ufast-pie>
     </div>
     <div class="li-pie">
-      <!-- <ufast-pie :pieConfig="pieConfig2" @change="change"></ufast-pie> -->
+      <ufast-pie :pieConfig="pieConfig2" @change="change"></ufast-pie>
     </div>
   </div>
 </template>
@@ -17,9 +17,49 @@ export default {
   },
   data() {
     return {
+      basePieConfig: {
+        width: "450px",
+        height: "350px",
+        startColor: ["#0157be", "#7a18ed", "#00bbce", "#ea865a", "#00bbce", "#ea865a"],
+        endColor: ["#0367d4", "#2743ed", "#00c4a5", "#ea2e41", "#00bbce", "#ea865a"],
+        borderStartColor: [
+          "#05acff",
+          "#ee36ff",
+          "#05fcfb",
+          "#ffa597",
+          "#00bbce",
+          "#ea865a",
+        ],
+        borderEndColor: [
+          "#09c1ff",
+          "#8171ff",
+          "#05ffff",
+          "#ff6584",
+          "#00bbce",
+          "#ea865a",
+        ],
+        legend: {
+          type: "plain",
+          orient: "vertical",
+          right: "5%",
+          top: "20%",
+          bottom: "20%",
+          icon: "circle",
+          itemGap: 30, //这个值是用来调图例之间的间距
+        },
+        title: {
+          text: "",
+          left: "13%",
+          top: "45%",
+          textStyle: {
+            lineHeight: 22,
+            fontWeight: "normal",
+            color: "#000",
+            fontSize: "20",
+          },
+        },
+      },
       pieConfig: {
-        width: "350px",
-        height: "250px",
         customTitle: "浏览量来源分布",
         switchTable: [
           { id: "1", name: "购电意向" },
@@ -27,31 +67,7 @@ export default {
           { id: "3", name: "零售套餐" },
           { id: "4", name: "用电需求" },
         ],
-        startColor: ["#0157be", "#7a18ed", "#00bbce", "#ea865a"],
-        endColor: ["#0367d4", "#2743ed", "#00c4a5", "#ea2e41"],
-        borderStartColor: ["#05acff", "#ee36ff", "#05fcfb", "#ffa597"],
-        borderEndColor: ["#09c1ff", "#8171ff", "#05ffff", "#ff6584"],
-        title: {
-          text: "购电意向浏览量\n      100人次",
-          left: "18%",
-          top: "45%",
-          textStyle: {
-            lineHeight: 20,
-            fontWeight: "normal",
-            color: "#000",
-            fontSize: "15",
-          },
-        },
-        legend: {
-          type: "scroll",
-          orient: "vertical",
-          right: "25%",
-          top: "20%",
-          bottom: "20%",
-          icon: "circle",
-          itemGap: 30, //这个值是用来调图例之间的间距
-        },
-         series: [
+        series: [
           {
             radius: ["55%", "65%"],
             center: ["40%", "50%"],
@@ -61,7 +77,7 @@ export default {
                 show: true,
                 position: "center",
                 formatter: () => {
-                  return 'xxxx';
+                  return "xxxx";
                 },
                 textStyle: {
                   fontSize: 40,
@@ -81,11 +97,7 @@ export default {
               },
             },
             name: "民警训练总量",
-            data: [
-              { value: 1048, name: "搜索引擎" },
-              { value: 735, name: "直接访问" },
-              { value: 580, name: "邮件营销" },
-            ],
+            data: [],
           },
           // 边框的设置
           {
@@ -112,17 +124,11 @@ export default {
             tooltip: {
               show: false,
             },
-            data: [
-              { value: 1048, name: "搜索引擎" },
-              { value: 735, name: "直接访问" },
-              { value: 580, name: "邮件营销" },
-            ],
+            data: [],
           },
         ],
       },
       pieConfig2: {
-        width: "350px",
-        height: "250px",
         customTitle: "浏览量来源分布",
         switchTable: [
           { id: "1", name: "购电意向" },
@@ -130,31 +136,7 @@ export default {
           { id: "3", name: "零售套餐" },
           { id: "4", name: "用电需求" },
         ],
-        startColor: ["#0157be", "#7a18ed", "#00bbce", "#ea865a"],
-        endColor: ["#0367d4", "#2743ed", "#00c4a5", "#ea2e41"],
-        borderStartColor: ["#05acff", "#ee36ff", "#05fcfb", "#ffa597"],
-        borderEndColor: ["#09c1ff", "#8171ff", "#05ffff", "#ff6584"],
-        title: {
-          text: "购电意向浏览量\n      100人次",
-          left: "18%",
-          top: "45%",
-          textStyle: {
-            lineHeight: 20,
-            fontWeight: "normal",
-            color: "#000",
-            fontSize: "15",
-          },
-        },
-        legend: {
-          type: "scroll",
-          orient: "vertical",
-          right: "25%",
-          top: "20%",
-          bottom: "20%",
-          icon: "circle",
-          itemGap: 30, //这个值是用来调图例之间的间距
-        },
-         series: [
+        series: [
           {
             radius: ["55%", "65%"],
             center: ["40%", "50%"],
@@ -164,7 +146,7 @@ export default {
                 show: true,
                 position: "center",
                 formatter: () => {
-                  return 'xxxx';
+                  return "xxxx";
                 },
                 textStyle: {
                   fontSize: 40,
@@ -184,11 +166,7 @@ export default {
               },
             },
             name: "民警训练总量",
-            data: [
-              { value: 1048, name: "搜索引擎" },
-              { value: 735, name: "直接访问" },
-              { value: 580, name: "邮件营销" },
-            ],
+            data: [],
           },
           // 边框的设置
           {
@@ -215,11 +193,7 @@ export default {
             tooltip: {
               show: false,
             },
-            data: [
-              { value: 1048, name: "搜索引擎" },
-              { value: 735, name: "直接访问" },
-              { value: 580, name: "邮件营销" },
-            ],
+            data: [],
           },
         ],
       },
@@ -228,11 +202,35 @@ export default {
   computed: {},
   //监控data中的数据变化
   watch: {},
-  created() {},
+  created() {
+    // this.pieConfig = JSON.parse(
+    //   JSON.stringify({ ...this.basePieConfig, ...this.pieConfig })
+    // );
+    this._getData();
+  },
   mounted() {},
   methods: {
     change(e) {
       console.log(e);
+    },
+    _getData() {
+      setTimeout(() => {
+        const data = [
+          { value: 1048, name: "搜索引擎" },
+          { value: 735, name: "直接访问" },
+          { value: 580, name: "邮件营销" },
+        ];
+        this.pieConfig.series[0].data = data;
+        this.pieConfig.series[1].data = data;
+        this.pieConfig = JSON.parse(
+          JSON.stringify({ ...this.basePieConfig, ...this.pieConfig })
+        );
+        this.pieConfig2.series[0].data = data;
+        this.pieConfig2.series[1].data = data;
+        this.pieConfig2 = JSON.parse(
+          JSON.stringify({ ...this.basePieConfig, ...this.pieConfig2 })
+        );
+      }, 1000);
     },
   },
 };
@@ -240,6 +238,8 @@ export default {
 <style lang="scss" scoped>
 .pie-content {
   width: 100%;
+  display: flex;
+  justify-content: space-between;
   .li-pie {
     margin: 20px;
     width: 40%;
